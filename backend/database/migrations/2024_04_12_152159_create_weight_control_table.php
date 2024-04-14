32266414248a5e6288a0e9cad2d690c40c6983f5
Rodrigo Lima <rodrigolima@brasilcard.net>
2024-04-14 14:52:20 -0300

    refact: change weight field to float to allow register correctly the weight

--- backend/database/migrations/2024_04_12_152159_create_weight_control_table.php
+++ backend/database/migrations/2024_04_12_152159_create_weight_control_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->bigInteger('user_id')->unsigned()->index()->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->integer('weight');
+            $table->float('weight');
             $table->timestamps();
         });
     }
