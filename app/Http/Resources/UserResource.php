4392326a7ea0af2a2e178f3d449fc79d4c02db6a
Rodrigo Lima <rodrigolima@brasilcard.net>
2024-05-13 10:59:27 -0300

    feat: add telegram link in user model and resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -16,18 +16,20 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this['id'] ?? null,
-            'name' => $this['name'],
-            'email' => $this['email'],
-            'password' => Hash::make($this['password']),
-            'phone' => $this['phone'] ?? null,
-            'gender' => $this['gender'] ?? null,
-            'dob' => $this['dob'] ?? null,
-            'height' => $this['height'] ?? null,
-            'weight' => $this['weight'] ?? null,
-            'daily_water_amount' => $this['daily_water_amount'] ?? null,
-            'activity_level' => $this['activity_level'] ?? null,
-            'image' => $this['image'] ?? null,
+            'id' => $this['id'] ?? ($this->id ?? null),
+            'name' => $this['name'] ?? $this->name,
+            'email' => $this['email'] ?? $this->email,
+            'password' => $this['password'] ? Hash::make($this['password']) : ($this->password ? Hash::make($this->password) : null),
+            'phone' => $this['phone'] ?? ($this->phone ?? null),
+            'gender' => $this['gender'] ?? ($this->gender ?? null),
+            'dob' => $this['dob'] ?? ($this->dob ?? null),
+            'height' => $this['height'] ?? ($this->height ?? null),
+            'weight' => $this['weight'] ?? ($this->weight ?? null),
+            'daily_water_amount' => $this['daily_water_amount'] ?? ($this->daily_water_amount ?? null),
+            'activity_level' => $this['activity_level'] ?? ($this->activity_level ?? null),
+            'image' => $this['image'] ?? ($this->image ?? null),
+            'telegram_user_id' => $this['telegram_user_id'] ?? ($this->telegram_user_id ?? null),
+            'telegram_user_deeplink' => $this['telegram_user_deeplink'] ?? ($this->telegram_user_deeplink ?? null),
         ];
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'phone', 'gender', 'dob', 'height', 'weight', 'daily_water_amount', 'activity_level', 'active', 'image'
+        'name', 'email', 'password', 'phone', 'gender', 'dob', 'height', 'weight', 'daily_water_amount', 'activity_level', 'active', 'image', 'telegram_user_id', 'telegram_user_deeplink'
     ];
 
     /**
