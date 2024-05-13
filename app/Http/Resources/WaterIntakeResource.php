a2b4c07404086ee6d86f40751446740caa45d703
Rodrigo Lima <rodrigolima@brasilcard.net>
2024-05-13 14:04:48 -0300

    fix: avoid call to undefined index

--- app/Http/Resources/WaterIntakeResource.php
+++ app/Http/Resources/WaterIntakeResource.php
@@ -15,11 +15,11 @@ class WaterIntakeResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
-            'user_id' => $this->user_id,
-            'amount' => $this->amount,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
+            'id' => isset($this['id']) ? $this['id'] : (isset($this->id) ? $this->id : null),
+            'user_id' => isset($this['user_id']) ? $this['user_id'] : (isset($this->user_id) ? $this->user_id : null),
+            'amount' => isset($this['amount']) ? $this['amount'] : (isset($this->amount) ? $this->amount : null),
+            'created_at' => isset($this['created_at']) ? $this['created_at'] : (isset($this->created_at) ? $this->created_at : null),
+            'updated_at' => isset($this['updated_at']) ? $this['updated_at'] : (isset($this->updated_at) ? $this->updated_at : null),
         ];
     }
 }
