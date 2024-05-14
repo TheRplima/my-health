ecf295d5e8ea45f97239f926f92b4ec092b37a35
Rodrigo Lima <rodrigolima@brasilcard.net>
2024-05-13 22:40:26 -0300

    feat: create config file to set some configs of the reminder

---
+++ config/water-intake-reminder.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'enabled' => env('WATER_INTAKE_REMINDER_ENABLED', true),
+    'interval' => env('WATER_INTAKE_REMINDER_INTERVAL', 60),
+    'snooze' => env('WATER_INTAKE_REMINDER_SNOOZE', 15),
+    'start' => env('WATER_INTAKE_REMINDER_START', '08:00'),
+    'end' => env('WATER_INTAKE_REMINDER_END', '23:00'),
+
+    'subscribable_notification_type' => [
+        'mail' => 'water-intake-reminder-mail',
+        'database' => 'water-intake-reminder-database',
+        'telegram' => 'water-intake-reminder-telegram',
+    ],
+
+    'subscribable_notification_type_description' => [
+        'mail' => 'Lembrete Ingestão de Água via e-mail',
+        'database' => 'Lembrete Ingestão de Água via notificação interna',
+        'telegram' => 'Lembrete Ingestão de Água via Telegram',
+    ],
+
+    'subscribable_notifications' => [
+        'water-intake-reminder-mail',
+        'water-intake-reminder-database',
+        'water-intake-reminder-telegram',
+    ],
+
+];
