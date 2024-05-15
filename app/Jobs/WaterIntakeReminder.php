407fc9fc216617740db28f344ce6c8bfce8bef3d
Rodrigo Lima <rodrigolima@brasilcard.net>
2024-05-15 00:58:37 -0300

    refact: modification on routine of water ingestiion reminder to use the new notification settings

--- app/Jobs/WaterIntakeReminder.php
+++ app/Jobs/WaterIntakeReminder.php
@@ -30,10 +30,6 @@ class WaterIntakeReminder implements ShouldQueue
     public function handle(): void
     {
         $enabled = config('water-intake-reminder.enabled');
-        $interval = config('water-intake-reminder.interval');
-        $snooze = config('water-intake-reminder.snooze');
-        $start = config('water-intake-reminder.start');
-        $end = config('water-intake-reminder.end');
 
         if (!$enabled) {
             return;
@@ -41,58 +37,40 @@ class WaterIntakeReminder implements ShouldQueue
 
         $users = User::all();
         foreach ($users as $user) {
-            // If the user has no notification subscriptions, skip
-            if (count($user->notificationSubscriptions) <= 0) {
-                continue;
-            }
-
-            // If the user has disabled notifications, skip
-            $disableNotification = Cache::get('disable_notification_' . $user->id);
-            if ($disableNotification === 0 || ($disableNotification && now()->diffInMinutes($disableNotification) < 0)) {
-                continue;
-            }
-
-            //if now is before 08:00 and after 23:00, skip
-            if (!now()->between($start, $end)) {
-                continue;
-            }
-
-            // If the user has not set a daily water amount, skip
-            $goal = $user->daily_water_amount;
-            if ($goal === null) {
-                continue;
-            }
-
-            // Get the amount ingested and the goal of the user. If the amount ingested is more than the goal, skip
-            $waterIntakesToday = $user->waterIntakeToday();
-            $amountIngested = $waterIntakesToday->sum('amount');
-            if ($amountIngested >= $goal) {
-                continue;
-            }
-
-            //if last registered drink is less than 1 hour ago, skip
-            $lastDrink = $waterIntakesToday->latest()->first();
-            if ($lastDrink && now()->diffInMinutes($lastDrink->created_at) < $interval) {
-                continue;
-            }
-
-            //if last notification sent to user is less than 15 minutes ago, skip
-            $lastNotification = Cache::get('lastest_notification_' . $user->id);
-            if ($lastNotification && now()->diffInMinutes($lastNotification) < $snooze) {
-                continue;
-            }
-
-
-            //send reminder
             $subscribeManagement = new NotificationSubscriptionManager();
             foreach ($user->notificationSubscriptions as $notificationSubscription) {
-                $channel = $subscribeManagement->subscribableNotificationClassFromType($notificationSubscription->type);
-                $user->notify(new $channel($user));
-                Log::info('Reminder sent to user ' . $user->id . ' via ' . $notificationSubscription->type, ['last_drink' => $lastDrink ? $lastDrink->created_at : null, 'last_notification' => $lastNotification ?? null, 'amount_ingested' => $amountIngested ?? 0, 'goal' => $goal]);
-            }
+                $type = $notificationSubscription->type;
+                $channel = $subscribeManagement->subscribableNotificationClassFromType($type);
+                $notificationSetting = $user->getNotificationSetting($type);
 
-            // Cache the last notification
-            Cache::put('lastest_notification_' . $user->id, now());
+                if ($user->isNotificationDisabled($type) || $user->isNotificationSnoozed($type)) {
+                    continue;
+                }
+
+                if (!now()->between($notificationSetting->start, $notificationSetting->end)) {
+                    continue;
+                }
+
+                $goal = $user->daily_water_amount;
+                if ($goal === null) {
+                    continue;
+                }
+
+                $waterIntakesToday = $user->waterIntakeToday();
+                $amountIngested = $waterIntakesToday->sum('amount');
+                if ($amountIngested >= $goal) {
+                    continue;
+                }
+
+                $lastDrink = $waterIntakesToday->latest()->first();
+                if ($lastDrink && now()->diffInMinutes($lastDrink->created_at) < $notificationSetting->interval) {
+                    continue;
+                }
+
+                $user->notify(new $channel($user));
+                $user->snoozeNotification($type, $notificationSetting->snooze);
+                Log::info('Reminder sent to user ' . $user->id . ' via ' . $notificationSubscription->type, ['last_drink' => $lastDrink ? $lastDrink->created_at : null, 'amount_ingested' => $amountIngested ?? 0, 'goal' => $goal]);
+            }
         }
     }
 }
