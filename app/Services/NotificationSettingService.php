9a89f7c945cf19a37d9661a9c21a48e0b16139f0
Rodrigo Lima <rodrigolima@brasilcard.net>
2024-05-19 12:16:50 -0300

    feat: added function to snooze and to disable all notifications of user

--- app/Services/NotificationSettingService.php
+++ app/Services/NotificationSettingService.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Response;
 use App\Repositories\NotificationSettingRepository;
 use App\Exceptions\FailedAction;
 use App\Http\Resources\NotificationSettingResource;
+use Illuminate\Support\Facades\Log;
 
 class NotificationSettingService
 {
@@ -55,12 +56,57 @@ class NotificationSettingService
         }
     }
 
+    public function disable(int $id)
+    {
+        try {
+            $this->notificationSettingRepository->disable($id);
+        } catch (\Exception $e) {
+            throw new FailedAction('Failed to disable notification setting. Error: ' . $e->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    public function snooze(int $id, int $minutes)
+    {
+        try {
+            $this->notificationSettingRepository->snooze($id, $minutes);
+        } catch (\Exception $e) {
+            throw new FailedAction('Failed to snooze notification setting. Error: ' . $e->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+    }
+
     public function enableAllFromUser($user_id)
     {
         try {
             $this->notificationSettingRepository->enableAllFromUser($user_id);
+
+            return true;
         } catch (\Exception $e) {
+            Log::error($e->getMessage());
             throw new FailedAction('Failed to enable all notification settings from user. Error: ' . $e->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    public function disableAllFromUser($user_id)
+    {
+        try {
+            $this->notificationSettingRepository->disableAllFromUser($user_id);
+
+            return true;
+        } catch (\Exception $e) {
+            throw new FailedAction('Failed to disable all notification settings from user. Error: ' . $e->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    public function snoozeAllFromUser($data)
+    {
+        $user_id = $data['user_id'];
+        $minutes = $data['minutes'];
+        try {
+            $this->notificationSettingRepository->snoozeAllFromUser($user_id, $minutes);
+
+            return true;
+        } catch (\Exception $e) {
+            throw new FailedAction('Failed to snooze all notification settings from user. Error: ' . $e->getMessage(), Response::HTTP_BAD_REQUEST);
         }
     }
 }
