fb2581479a7ecf49ed6d35f68133eaee61e8af71
Rodrigo Lima <rodrigolima@brasilcard.net>
2024-05-19 12:20:27 -0300

    feat: add function to return list to bot

--- app/Services/WaterIntakeService.php
+++ app/Services/WaterIntakeService.php
@@ -2,12 +2,13 @@
 
 namespace App\Services;
 
-use App\Exceptions\FailedAction;
 use App\Hooks\SendCallbackQueryHomeAssistant;
 use App\Http\Resources\WaterIntakeCollection;
 use App\Repositories\WaterIntakeRepository;
 use App\Http\Resources\WaterIntakeResource;
+use App\Exceptions\FailedAction;
 use Illuminate\Http\Response;
+use App\Models\User;
 
 class WaterIntakeService
 {
@@ -76,5 +77,41 @@ class WaterIntakeService
         return new WaterIntakeCollection(
             $this->waterIntakeRepository->getWaterIntakesByDay($userId, $date)
         );
+    }
+
+    public function showWaterIntakeToday(User $user)
+    {
+
+        $waterintakes = $this->getWaterIntakesByDay($user->id, now()->toDateString());
+        if ($waterintakes) {
+            $total = $waterintakes->sum('amount');
+            $list = '';
+            foreach ($waterintakes as $waterintake) {
+                $list .= $waterintake->created_at->format('H:i') . ' \- ';
+                $list .= $waterintake->amount . 'ml' . "\n";
+            }
+
+            if (!$user->daily_water_amount) {
+                $message = "Você ainda não definiu sua meta de consumo diário de água\. Para definir sua meta de consumo diário de água acesse o menu Perfil de Usuário\.\n";
+            } else {
+                $message = "Sua meta de consumo diário é de *{$user->daily_water_amount} ml*\n";
+            }
+
+            $message .= "Você já consumiu *{$total}ml* de água hoje\.\n";
+
+            if ($total >= $user->daily_water_amount) {
+                $message .= "Parabéns! Você já atingiu sua meta de consumo diário de água\.\n";
+            } else {
+                $missing = $user->daily_water_amount - $total;
+                $message .= "Faltam *{$missing}ml* para atingir sua meta de consumo diário de água\.\n";
+            }
+            if ($list) {
+                $message .= "\nDetalhes:\n" . $list;
+            }
+
+            return $message;
+        }
+
+        return 'Você ainda não consumiu água hoje.';
     }
 }
